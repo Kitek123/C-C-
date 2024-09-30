407d9f789e31250d796e6d66a58247cdf979f576
Bongo664 <patrykpojda@gmail.com>
2024-09-30 08:33:54 +0200

    1

---
+++ 30.09.24/zad1/zad1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+/*
+1.Klasa Prostokąt: która ma prywatne pola szerokosc i wysokosc
+dodaj metody do ustawiania i pobierania wartości tych pól
+oraz metodę do obliczania pola prostokąta.
+*/
+
+class Prostokat {
+private:
+	double szerokosc, wysokosc;
+public:
+	void ustawSzerplpsc(double s) { this->szerokosc = s; }
+	void ustawWysokosc(double w) { this->wysokosc = w; }
+	double obliczPole() { return this->szerokosc * this->wysokosc;}
+};
+
+int main()
+{
+	cout << "Zadanie nr1\n";
+	Prostokat prostokat;
+	prostokat.ustawSzerplpsc(10.23);
+	prostokat.ustawWysokosc(4.45);
+	cout << "Wynik Pole prostokata = " << prostokat.obliczPole();
+
+	return EXIT_SUCCESS;
+
+}
---
+++ 30.09.24/zad2/zad2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace  std;
+/*
+Dziedziczenie: Stworz klase Pojazd z polami nazwa,
+predkosc i metodą jazd. Nastepnie utwórz klase Samochod,
+ktora dzieziczy po Pojazd i dodaj do niej pole LiczbaDrzwi
+*/
+class Pojazd {
+public:
+	string nazwa;
+	int predkosc;
+	void jedz() {
+		cout << "Pojazd jedzie z predkoscia " << this->predkosc << endl;
+	}
+};
+class Samochod :public Pojazd{
+public:
+	int liczbaDrzwi;
+	void opisSamochodu() { cout << "ilosc drzwi " << this->liczbaDrzwi << endl; }
+};
+int main()
+{
+	cout << "Zadabue nr 2\n";
+	Samochod fiat;
+	fiat.nazwa = "FIAT 125p - KANCIOK";
+	fiat.liczbaDrzwi = 5;
+	fiat.predkosc = 120;
+	fiat.opisSamochodu();
+	fiat.jedz();
+	return 0;
+}
