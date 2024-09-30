60163af233e44e711d63f38541a948ec2d1504dc
Bongo664 <patrykpojda@gmail.com>
2024-09-30 08:46:29 +0200

    2

---
+++ 30.09.24/zad3/zad3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace  std;
+/*
+3. Polimorfizm: Stworz klase Figura, kwadrat i kolo. zdefiniuj w klasie Figura wirtualna metode
+ktora adpiszesz w klasach kwadrat i kolo
+*/
+class Figura {
+public:
+	virtual double obliczPole() = 0; 
+};
+class Kwadrat : public Figura {
+private:
+	double bok;
+public:
+};
+class Kolo : public Figura {};
+int main()
+{
+	cout << "Zadanie nr 3 - Polimorfizm\n";
+
+	return 0;
+}
